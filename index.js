283b859ce43b05eec1285b3eacd3d734470bf6ce
unknown <imed.siala@gmail.com>
2023-11-07 14:02:48 +0100

    serveurfff

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ const app = express();
 const __filename = url.fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 app.use(express.json({limit: '50mb'}));
-app.use(express.urlencoded({limit: '50mb'}));
+app.use(express.urlencoded());
 app.use(helmet());
 app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
